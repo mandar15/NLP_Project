be269e2044cca907b0252b2e5b03ff815161389f
mandar15 <paingankar.mandar@gmail.com>
2012-11-11 18:37:43 -0500

    Added a script for cleaning Test and Training Dirs.

---
+++ NLP_Project/scripts/clean_dirs.pl
@@ -0,0 +1,13 @@
+#
+# SCRIPT to Clean test and training files from test and train folders.
+#
+
+system("rm -rf ../train/pos/*");
+system("rm -rf ../train/bow/*");
+system("rm -rf ../train/ngram/*");
+system("rm -rf ../test/pos/*");
+system("rm -rf ../test/bow/*");
+system("rm -rf ../test/ngram/*");
+
+
+
