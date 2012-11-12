8edd9325dab4c8954ea0faf014137db35762945d
root <root@ubuntu.(none)>
2012-11-11 21:58:27 -0500

    added files

--- NLP_Project/src/nlp/POS/PosMain.java
+++ NLP_Project/src/nlp/POS/PosMain.java
@@ -8,31 +8,69 @@ import nlp.utilities.Constants;
 public class PosMain {
 
 	public static void main(String args[]) throws IOException{
-		Constants constObj = new Constants();
+		genTwitterFiles(5, 0 , 400 , 500);
+		
+		genBlogData(5 , 0 , 160 , 200);
+	}
+	
+	public static void genBlogData(int botNum, int start, int trainEnd, int end) throws IOException {
+		// TODO Auto-generated method stub
+Constants constObj = new Constants();
 		
 		POSGenAttribute obj = new POSGenAttribute();
 		
-		for(int i =1; i <= 5; i++){
-			for(int j = i + 1; j <= 5; j++){
+		for(int i =1; i <= botNum; i++){
+			for(int j = i + 1; j <= botNum; j++){
 				if( i == j)
 					continue;
-				obj.readFilePopulateTags("data/tweet/TwBot" + i, 0 , 400);
-				obj.readFilePopulateTags("data/tweet/TwBot" + j, 0 , 400);
+				obj.readFilePopulateTags("data/blog/Bot" + i, start , trainEnd);
+				obj.readFilePopulateTags("data/blog/Bot" + j, start , trainEnd);
 				
 				FileWriter outTrainFile = new FileWriter(constObj.getTrainFilePrefixPos() + "pos." + i + "_" + j + ".trn");
-				obj.genFile("data/tweet/TwBot" + i,outTrainFile,0, 400);
+				obj.genFile("data/blog/Bot" + i,outTrainFile,start, trainEnd);
 				outTrainFile.close();
 				outTrainFile = new FileWriter(constObj.getTrainFilePrefixPos() + "pos." + i + "_" + j + ".trn",true);
-				obj.genFile("data/tweet/TwBot" + j,outTrainFile,0, 400);
+				obj.genFile("data/blog/Bot" + j,outTrainFile,start , trainEnd);
 				outTrainFile.close();
 				
 				
 				FileWriter outTestFile = new FileWriter(constObj.getTestFilePrefixPos() + "pos." + i + "_" + j + ".tst");
-				obj.genFile("data/tweet/TwBot" + i,outTestFile, 400, 500);
+				obj.genFile("data/blog/Bot" + i,outTestFile, trainEnd, end);
 				outTestFile.close();
 				
 				FileWriter outTestFile2 = new FileWriter(constObj.getTestFilePrefixPos() + "pos." + j + "_" + i + ".tst");
-				obj.genFile("data/tweet/TwBot" + j, outTestFile2, 400, 500);
+				obj.genFile("data/blog/Bot" + j, outTestFile2, trainEnd, end);
+				outTestFile2.close();
+			}
+		}
+	}
+
+	public static void genTwitterFiles(int botNum, int start, int trainEnd, int end) throws IOException{
+		Constants constObj = new Constants();
+		
+		POSGenAttribute obj = new POSGenAttribute();
+		
+		for(int i =1; i <= botNum; i++){
+			for(int j = i + 1; j <= botNum; j++){
+				if( i == j)
+					continue;
+				obj.readFilePopulateTags("data/tweet/TwBot" + i, start , trainEnd);
+				obj.readFilePopulateTags("data/tweet/TwBot" + j, start , trainEnd);
+				
+				FileWriter outTrainFile = new FileWriter(constObj.getTrainFilePrefixPos() + "pos." + i + "_" + j + ".trn");
+				obj.genFile("data/tweet/TwBot" + i,outTrainFile,start, trainEnd);
+				outTrainFile.close();
+				outTrainFile = new FileWriter(constObj.getTrainFilePrefixPos() + "pos." + i + "_" + j + ".trn",true);
+				obj.genFile("data/tweet/TwBot" + j,outTrainFile,start , trainEnd);
+				outTrainFile.close();
+				
+				
+				FileWriter outTestFile = new FileWriter(constObj.getTestFilePrefixPos() + "pos." + i + "_" + j + ".tst");
+				obj.genFile("data/tweet/TwBot" + i,outTestFile, trainEnd, end);
+				outTestFile.close();
+				
+				FileWriter outTestFile2 = new FileWriter(constObj.getTestFilePrefixPos() + "pos." + j + "_" + i + ".tst");
+				obj.genFile("data/tweet/TwBot" + j, outTestFile2, trainEnd, end);
 				outTestFile2.close();
 			}
 		}
